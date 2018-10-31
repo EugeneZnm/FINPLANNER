af6e051cd0c05638c3b1bffb23a63237674e18a4
TonyKioko <tonykioko384@gmail.com>
2018-10-31 17:19:59 +0300

    add login and register view functions

--- finplanner/urls.py
+++ finplanner/urls.py
@@ -5,5 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
 
 ]
