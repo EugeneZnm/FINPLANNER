7741c242514d4b3392c3ea61da9b365ead0342d4
TonyKioko <tonykioko384@gmail.com>
2018-11-07 22:24:09 +0300

    analytics view created

--- finplanner/views.py
+++ finplanner/views.py
@@ -234,7 +234,7 @@ class ExpenseCreate(CreateView):
         return HttpResponseRedirect(reverse_lazy('finplanner:expenses'))
 class ExpenseUpdate(UpdateView):
     template_name = "expense_form.html"
-    
+
     model = Expense
     fields = [
         'date',
@@ -243,6 +243,277 @@ class ExpenseUpdate(UpdateView):
         'payment',
         'amount'
     ]
+
+
+class AnalyticsView(generic.ListView):
+    template_name = "analytics/index.html"
+    context_object_name = "records"
+    model = Expense
+
+    def annually(request, year):
+        # get user expense objects
+        exp = Expense.objects.filter(created_by=request.user)
+
+        # retrieve distinct types
+        expense_type = list(exp.filter(date__year=year).values('type').distinct().order_by()
+                                 .values_list('type', flat=True))
+
+        datasets = []
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                  "November", "December"]
+
+        for type in expense_type:
+            # expense
+            arr = list(exp.filter(date__year=year).filter(type=type).distinct().order_by('date'))
+
+            # expense month by type
+            month_tmp_arr = list(exp.filter(date__year=year).filter(type=type).values('date').distinct()
+                            .order_by('date')
+                            .values_list('date', flat=True))
+
+            # expense amount by type
+            expense_tmp_arr = list(exp.filter(date__year=year).filter(type=type)
+                .values('date').distinct().order_by('date')
+                .annotate(amount=Sum('amount')).values_list('amount', flat=True))
+
+            # init array with size 12 with value 0
+            monthly_expense_cnt = [0] * 12
+
+            for i, m in enumerate(arr):
+                monthly_expense_cnt[m.date.month-1] += m.amount
+
+            total_amount = []
+
+            for j, k in enumerate(monthly_expense_cnt):
+                o = {}
+                o['x'] = months[j];
+                o['y'] = monthly_expense_cnt[j];
+                total_amount.append(o)
+
+            # generate random color
+            r = lambda: random.randint(0, 255)
+            color = '#%02X%02X%02X' % (r(), r(), r())
+
+            # construct dataset
+            dataset = {
+                'label': type,
+                'backgroundColor': color,
+                'borderColor': color,
+                'data': total_amount,
+                'fill': 'false'
+            }
+
+            datasets.append(dataset)
+
+        # fetch available years for menu labels
+        dates = list(exp.values('date')
+             .values_list('date', flat=True))
+        year_arr = []
+        for date in dates:
+            if date.year not in year_arr:
+                year_arr.append(date.year)
+        year_arr.sort()
+
+        # construct submenu
+        expense_per_month = []
+        month_not_none = []
+        for i, month in enumerate(months):
+            amount = exp.filter(date__year=year).filter(date__month=(i+1)).values('date').distinct().order_by('date').aggregate(amount=Sum('amount'))['amount']
+            if amount is not None:
+                expense_per_month.append(float("{0:.2f}".format(amount)))
+                month_not_none.append((months[i]))
+
+
+        if expense_per_month:
+            submenu = zip(month_not_none, expense_per_month)
+        else:
+            submenu = False
+
+        context = {
+            'context_type': 'annually',
+            'datasets': datasets,
+            'labels': months,
+            'title': 'Annual Report in ' + str(year),
+            'report_type': 'line',
+            'menu_labels': year_arr,
+            'x_axis_label': 'Month',
+            'submenu': submenu
+        }
+
+        return render(request, "analytics/index.html", context)
+
+    def monthly(request, year, month):
+        # get user expense objects
+        exp = Expense.objects.filter(created_by=request.user)
+
+        expense_table_in_month_view = ExpenseTable(exp.filter(date__year=year).filter(date__month=month))
+        expense_table_in_month_view.paginate(page=request.GET.get('page', 1), per_page=10)
+
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                  "November", "December"]
+        months_reverse = dict(January=1, February=2, March=3, April=4)
+        datasets = []
+
+        # retrieve distinct types
+        expense_type = list(exp.filter(date__year=year).filter(date__month=month).values('type').distinct().order_by()
+                            .values_list('type', flat=True))
+
+        lastday = monthrange(year, month)[1]
+
+        days = list(range(1,lastday+1))
+
+        for type in expense_type:
+            # expense
+            expense_queryset = exp.filter(date__year=year).filter(date__month=month).filter(type=type).distinct().order_by('date')
+            arr = list(expense_queryset)
+
+            # expense month by type
+            month_tmp_arr = list(exp.filter(date__year=year).filter(date__month=month).filter(type=type).values('date').distinct()
+                                 .order_by('date')
+                                 .values_list('date', flat=True))
+
+            # expense amount by type
+            expense_tmp_arr = list(exp.filter(date__year=year).filter(date__month=month).filter(type=type)
+                                   .values('date').distinct().order_by('date')
+                                   .annotate(amount=Sum('amount')).values_list('amount', flat=True))
+
+            # init array
+            daily_expense_cnt = [0] * lastday
+            for i, m in enumerate(arr):
+                daily_expense_cnt[m.date.day - 1] += m.amount
+
+            total_amount = []
+            for j, k in enumerate(daily_expense_cnt[1:]):
+                o = {}
+                o['x'] = days[j];
+                o['y'] = daily_expense_cnt[j];
+                total_amount.append(o)
+
+            # generate random color
+            r = lambda: random.randint(0, 255)
+            color = '#%02X%02X%02X' % (r(), r(), r())
+
+            # construct dataset
+            dataset = {
+                'label': type,
+                'backgroundColor': color,
+                'borderColor': color,
+                'data': total_amount,
+                'fill': 'false'
+            }
+
+            datasets.append(dataset)
+
+        # fetch available months for menu labels
+        dates = list(exp.values('date')
+                     .values_list('date', flat=True))
+        month_arr = []
+        month_labels = []
+        for date in dates:
+            if date.month not in month_arr:
+                month_arr.append(date.month)
+                month_arr.sort()
+
+        for single_month in month_arr:
+            month_labels.append(months[single_month - 1])
+        # construct submenu
+        expense_per_day = []
+        day_not_none  = []
+        for i, d in enumerate(days):
+            amount = exp.filter(date__year=year).filter(date__month=month).filter(date__day=(i+1)).values('date').distinct().order_by(
+                'date').aggregate(amount=Sum('amount'))['amount']
+            if amount is not None:
+                expense_per_day.append(float("{0:.2f}".format(amount)))
+                day_not_none.append((i+1))
+
+        if expense_per_day:
+            submenu = zip(day_not_none, expense_per_day)
+        else:
+            submenu = False
+
+        context = {
+            'context_type': 'monthly',
+            'datasets': datasets,
+            'monthly_expense': expense_type,
+            'labels': days,
+            'title': 'Monthly Report on ' + str(months[month-1]) + ' ' + str(year),
+            'report_type': 'bar',
+            'selected_year':year ,
+            'menu_labels': month_arr,
+            'month_labels': month_labels,
+            'x_axis_label': 'Day',
+            'submenu': submenu,
+            'months': months,
+            'expense_table_in_month_view': expense_table_in_month_view
+        }
+        return render(request, "analytics/index.html", context)
+
+    def daily(request, year, month, day):
+        # get user expense objects
+        exp = Expense.objects.filter(created_by=request.user)
+
+        # expense table
+        expense_table_in_daily_view = ExpenseTable(exp.filter(date__year=year).filter(date__month=month).filter(date__day=day))
+        expense_table_in_daily_view.paginate(page=request.GET.get('page', 1), per_page=10)
+
+        type =[]
+        # retrieve distinct types
+        expense_type = list(
+            exp.filter(date__year=year).filter(date__month=month)
+            .filter(date__day=day).values('type').distinct().order_by()
+            .values_list('type', flat=True))
+
+        datasets = []
+        expense_arr = []
+        color_arr = []
+        for type in expense_type:
+            # expense amount by type
+            expense_tmp_arr = list(exp.filter(date__year=year).filter(date__month=month).filter(date__day=day)
+                                   .filter(type=type)
+                                   .values('date').distinct().order_by('date')
+                                   .annotate(amount=Sum('amount')).values_list('amount', flat=True))
+            # get the sum
+            expense_arr.append(expense_tmp_arr[0])
+
+            # generate random color
+            r = lambda: random.randint(0, 255)
+            color = '#%02X%02X%02X' % (r(), r(), r())
+            color_arr.append(color)
+
+        # construct dataset
+        if color_arr and expense_arr:
+            dataset = {
+                'label': type,
+                'backgroundColor': color_arr,
+                'borderColor': color_arr,
+                'data': expense_arr
+            }
+            datasets.append(dataset)
+
+        # construct submenu
+        expense_per_type = []
+        for i, type in enumerate(expense_type):
+            amount = exp.filter(date__year=year).filter(date__month=month).filter(date__day=day).filter(type=type).values('date').distinct().order_by('date').aggregate(amount=Sum('amount'))['amount']
+            if amount is not None:
+                expense_per_type.append(float("{0:.2f}".format(amount)))
+
+        if expense_per_type:
+            submenu = zip(expense_type, expense_per_type)
+        else:
+            submenu = False
+
+        context = {
+            'context_type': 'daily',
+            'datasets': datasets,
+            'daily_expense': expense_type,
+            'labels': expense_type,
+            'title': 'Daily Report on ' + str(day) + '/' + str(month) + '/' + str(year) ,
+            'x_axis_label': 'Type',
+            'report_type': 'pie',
+            'submenu': submenu,
+            'expense_table_in_daily_view': expense_table_in_daily_view
+        }
+        return render(request, "analytics.html", context)
 
 
 
