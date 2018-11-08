86756af4fb7f61383522fd29e9bf7e2e11da95a9
TonyKioko <tonykioko384@gmail.com>
2018-11-08 14:29:35 +0300

    chjarts now showing

--- finproject/settings.py
+++ finproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'finplanner.templatetags.month_labels',
 ]
 
 MIDDLEWARE = [
